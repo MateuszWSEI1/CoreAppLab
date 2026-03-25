3dc8b569fcaf0c9858b11b2de485762ee51324cb
Mateusz Mechla <mateusz.mechla@microsoft.wsei.edu.pl>
2026-03-25 01:22:22 +0100

    Lab2

---
+++ UnitTest/MemoryGenericRepositoryTest.cs
@@ -0,0 +1,31 @@
+﻿using CoreApp.Entities;
+using CoreApp.Repositories;
+using Infrastructure.Memory;
+using Xunit;
+
+namespace UnitTest;
+
+public class MemoryGenericRepositoryTest
+{
+    private IGenericRepositoryAsync<Person> _repo = new MemoryGenericRepository<Person>();
+
+    [Fact]
+    public async Task AddPersonTestAsync()
+    {
+        // Arrange
+        var expected = new Person()
+        {
+            FirstName = "Adam",
+            LastName = "Kowalski"
+        };
+
+        // Act
+        await _repo.AddAsync(expected);
+
+        // Assert
+        var actual = await _repo.FindByIdAsync(expected.Id);
+
+        Assert.Equal(expected.Id, actual?.Id);
+        Assert.Equal(expected.FirstName, actual?.FirstName);
+    }
+}
