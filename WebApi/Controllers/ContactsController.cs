ba942d60642e06cde17e8b565fc714dee0cb8a69
Mateusz Mechla <mateusz.mechla@microsoft.wsei.edu.pl>
2026-03-25 04:19:32 +0100

    Definicja metody do edycji osoby

--- WebApi/Controllers/ContactsController.cs
+++ WebApi/Controllers/ContactsController.cs
@@ -1,4 +1,5 @@
-﻿using CoreApp.Services;
+﻿using CoreApp.Dto;
+using CoreApp.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers;
@@ -15,9 +16,43 @@ public class ContactsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAllPersons(int page = 1, int size = 10)
+    public async Task<IActionResult> GetAllPersons([FromQuery] int page = 1, [FromQuery] int size = 10)
     {
         var result = await _service.FindAllPeoplePaged(page, size);
+        return Ok(result);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetPerson(Guid id)
+    {
+        var dto = await _service.FindByIdAsync(id);
+
+        if (dto == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(dto);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreatePersonDto dto)
+    {
+        var result = await _service.CreateAsync(dto);
+        return CreatedAtAction(nameof(GetPerson), new { id = result.Id }, result);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Update(Guid id, UpdatePersonDto dto)
+    {
+        var existingPerson = await _service.FindByIdAsync(id);
+        
+        if (existingPerson == null)
+        {
+            return NotFound();
+        }
+
+        var result = await _service.UpdateAsync(id, dto);
         return Ok(result);
     }
 }
