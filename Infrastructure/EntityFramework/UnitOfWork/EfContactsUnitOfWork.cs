e85ed4b048e5e733ec90a56e5756e463896fc186
Mateusz Mechla <mateusz.mechla@microsoft.wsei.edu.pl>
2026-04-22 00:36:53 +0200

    Definicja UnitOfWork

---
+++ Infrastructure/EntityFramework/UnitOfWork/EfContactsUnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using CoreApp.Repositories;
+using Infrastructure.EntityFramework.Context;
+
+namespace Infrastructure.EntityFramework.UnitOfWork;
+
+public class EfContactsUnitOfWork(
+    IPersonRepositoryAsync personRepository,
+    ICompanyRepositoryAsync companyRepository,
+    IOrganizationRepositoryAsync organizationRepository,
+    IContactRepositoryAsync contactRepository,
+    ContactsDbContext context
+) : IContactUnitOfWork
+{
+    public ValueTask DisposeAsync()
+    {
+        return context.DisposeAsync();
+    }
+
+    public IPersonRepositoryAsync Persons => personRepository;
+    public ICompanyRepositoryAsync Companies => companyRepository;
+    public IOrganizationRepositoryAsync Organizations => organizationRepository;
+    public IContactRepositoryAsync Contacts => contactRepository;
+
+    public Task<int> SaveChangesAsync()
+    {
+        return context.SaveChangesAsync();
+    }
+
+    public Task BeginTransactionAsync()
+    {
+        return context.Database.BeginTransactionAsync();
+    }
+
+    public Task CommitTransactionAsync()
+    {
+        return context.Database.CommitTransactionAsync();
+    }
+
+    public Task RollbackTransactionAsync()
+    {
+        return context.Database.RollbackTransactionAsync();
+    }
+}
