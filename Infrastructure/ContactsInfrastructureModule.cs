36d751fb82f90b8b3aefedc39d7a6d558d4e4b18
Mateusz Mechla <mateusz.mechla@microsoft.wsei.edu.pl>
2026-04-22 00:55:15 +0200

    Konfiguracja modułu WebApi

---
+++ Infrastructure/ContactsInfrastructureModule.cs
@@ -0,0 +1,63 @@
+﻿using CoreApp.Repositories;
+using CoreApp.Services;
+using Infrastructure.EntityFramework.Context;
+using Infrastructure.EntityFramework.Entities;
+using Infrastructure.EntityFramework.Repositories;
+using Infrastructure.EntityFramework.UnitOfWork;
+using Infrastructure.Memory;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class ContactsInfrastructureModule
+{
+    public static IServiceCollection AddContactsEfModule(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddScoped<IPersonRepositoryAsync, EfPersonRepository>();
+        services.AddScoped<ICompanyRepositoryAsync, EfCompanyRepository>();
+        services.AddScoped<IOrganizationRepositoryAsync, EfOrganizationRepository>();
+        services.AddScoped<IContactRepositoryAsync, EfContactRepository>();
+
+        services.AddScoped<IContactUnitOfWork, EfContactsUnitOfWork>();
+
+        services.AddDbContext<ContactsDbContext>(options =>
+            options.UseSqlite(configuration.GetConnectionString("CrmDb")));
+
+        services.AddIdentity<CrmUser, CrmRole>(options =>
+            {
+                options.Password.RequiredLength = 8;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = true;
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+            })
+            .AddEntityFrameworkStores<ContactsDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddScoped<IPersonService, PersonService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddContactsMemoryModule(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddScoped<IPersonRepositoryAsync, MemoryPersonRepository>();
+        services.AddScoped<ICompanyRepositoryAsync, MemoryCompanyRepository>();
+        services.AddScoped<IOrganizationRepositoryAsync, MemoryOrganizationRepository>();
+        services.AddScoped<IContactRepositoryAsync, MemoryContactRepository>();
+
+        services.AddScoped<IContactUnitOfWork, MemoryContactUnitOfWork>();
+        services.AddScoped<IPersonService, PersonService>();
+
+        return services;
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,26 +1,15 @@
 using CoreApp.Module;
-using CoreApp.Repositories;
-using CoreApp.Services;
-using Infrastructure.Memory;
+using Infrastructure;
 using WebApi.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddContactsEfModule(builder.Configuration);
 builder.Services.AddContactsModule(builder.Configuration);
 
 builder.Services.AddControllers();
-
 builder.Services.AddOpenApi();
-
-builder.Services.AddSingleton<IPersonRepositoryAsync, MemoryPersonRepository>();
-builder.Services.AddSingleton<ICompanyRepositoryAsync, MemoryCompanyRepository>();
-builder.Services.AddSingleton<IOrganizationRepositoryAsync, MemoryOrganizationRepository>();
-builder.Services.AddSingleton<IContactRepositoryAsync, MemoryContactRepository>();
-
-builder.Services.AddSingleton<IContactUnitOfWork, MemoryContactUnitOfWork>();
-
-builder.Services.AddSingleton<IPersonService, PersonService>();
 
 builder.Services.AddExceptionHandler<ProblemDetailsExceptionHandler>();
 builder.Services.AddProblemDetails();
@@ -32,7 +21,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-//app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseAuthorization();
 app.UseExceptionHandler();
 app.MapControllers();
