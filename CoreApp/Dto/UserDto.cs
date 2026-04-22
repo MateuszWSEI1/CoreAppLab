6d368a91cffc38ef998b260ee7bb23c2f042710b
Mateusz Mechla <mateusz.mechla@microsoft.wsei.edu.pl>
2026-04-22 03:43:49 +0200

    Tworzenie AuthService

--- CoreApp/Dto/UserDto.cs
+++ CoreApp/Dto/UserDto.cs
@@ -1,7 +1,14 @@
-﻿namespace CoreApp.Dto;
+﻿using CoreApp.Enums;
+
+namespace CoreApp.Dto;
 
 public record UserDto
 {
-    public Guid Id { get; init; }
+    public string Id { get; init; } = string.Empty;
     public string Email { get; init; } = string.Empty;
+    public string FirstName { get; init; } = string.Empty;
+    public string LastName { get; init; } = string.Empty;
+    public SystemUserStatus Status { get; init; }
+    public string Department { get; init; } = string.Empty;
+    public IList<string> Roles { get; init; } = new List<string>();
 }
